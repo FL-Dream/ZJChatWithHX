893d2ba266533037d96c4bf02f27b9a6fdc60189
FL-Dream <xuan.xue.ai@qq.com>
2017-03-06 17:35:56 +0800

    更改框架名字并添加环信SDK

---
+++ ZJChatWithHX/Classes（工作区）/Login（登录）/Controller/ZJLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJLoginViewController.h
+//  ZJChatWithHX
+//
+//  Created by ZJ on 2017/3/6.
+//  Copyright © 2017年 周君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJLoginViewController : UIViewController
+
+@end
