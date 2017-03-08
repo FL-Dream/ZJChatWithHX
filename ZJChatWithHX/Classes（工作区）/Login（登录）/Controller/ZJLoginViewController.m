4a60d1046457d519b5da3f56d6fd84e077b6fc3b
FL-Dream <xuan.xue.ai@qq.com>
2017-03-08 18:32:48 +0800

    添加登录页面

--- ZJChatWithHX/Classes（工作区）/Login（登录）/Controller/ZJLoginViewController.m
+++ ZJChatWithHX/Classes（工作区）/Login（登录）/Controller/ZJLoginViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ZJLoginViewController.h"
+#import "ZJTabBarViewController.h"
 
 @interface ZJLoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *userNameField;
+@property (weak, nonatomic) IBOutlet UITextField *passWordField;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -19,19 +25,66 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)login:(UIButton *)sender
+{
+    
+    if ([_loginButton.currentTitle isEqualToString:@"登录"] )
+    {
+        if (_userNameField.text != nil && _passWordField.text != nil)
+        {
+            EMError *error = [[EMClient sharedClient] loginWithUsername:_userNameField.text password:_passWordField.text];
+            if (!error) {
+                [[EMClient sharedClient].options setIsAutoLogin:YES];
+                [ToolsObject showSuccessMessage:@"登录成功"];
+                SaveData(@"已登录", @"登录状态");
+                [self presentViewController:[[ZJTabBarViewController alloc] init] animated:YES completion:nil];
+            }
+            else
+            {
+                [ToolsObject showErrorMessage:error.errorDescription];
+            }
+            
+        }
+        else
+        {
+            [ToolsObject showErrorMessage:@"请输入账户密码！"];
+        }
+    }
+    else
+    {
+        _userNameField.text = nil;
+        _passWordField.text = nil;
+        [_loginButton setTitle:@"登录" forState:UIControlStateNormal];
+    }
+    
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)registerUser:(UIButton *)sender
+{
+    if ([_loginButton.currentTitle isEqualToString:@"返回"] )
+    {
+        if (_userNameField.text != nil && _passWordField.text != nil)
+        {
+            EMError *error = [[EMClient sharedClient] registerWithUsername:_userNameField.text password:_passWordField.text];
+            if (!error) {
+                [ToolsObject showSuccessMessage:@"注册成功"];
+                [_loginButton setTitle:@"登录" forState:UIControlStateNormal];
+            }
+            else
+            {
+                [ToolsObject showErrorMessage:error.errorDescription];
+            }
+            
+        }
+    }
+    else
+    {
+        _userNameField.text = nil;
+        _passWordField.text = nil;
+        [_loginButton setTitle:@"返回" forState:UIControlStateNormal];
+    }
+    
 }
-*/
 
 @end
